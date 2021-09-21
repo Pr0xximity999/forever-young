5438d51d51b4f8b143a4cc640ded719127ec6b9c
Thomas Koman <99066789@mydavinci.nl>
2021-09-21 14:18:40 +0200

    im gud at dutch apperantly

--- robotarm-python-2021-main/opdr 6.py
+++ robotarm-python-2021-main/opdr 6.py
@@ -6,13 +6,18 @@ robotArm.speed = 3
 def move(i, direction):
     if direction == "right":
         for x in range(0, i):
-            robotArm.moveLeft()
+            robotArm.moveRight()
     if direction == "left":
         for x in range(0, i):
             robotArm.moveLeft()
 
 # Jouw python instructies zet je vanaf hier:
-move(8, "right")
+move(7, "right")
+for i in range(0, 8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    move(2, "left")
 
 
 
